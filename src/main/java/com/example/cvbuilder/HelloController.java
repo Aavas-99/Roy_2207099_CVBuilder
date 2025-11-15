6d8349b20bdc506d181f384f59b918c9385eb5a9
Aavas Roy <roy2207099@stud.kuet.ac.bd>
2025-11-16 01:23:54 +0600

    Swithced from Welcome Scene to Next Scene

--- src/main/java/com/example/cvbuilder/HelloController.java
+++ src/main/java/com/example/cvbuilder/HelloController.java
@@ -2,7 +2,14 @@ package com.example.cvbuilder;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -13,6 +20,12 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    public void onButtonClicked(ActionEvent actionEvent) {
+    public void onButtonClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        Parent root= fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+
     }
 }
---
+++ src/main/java/com/example/cvbuilder/MainPageController.java
@@ -0,0 +1,17 @@
+package com.example.cvbuilder;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainPageController {
+
+    @FXML
+    private TextField FullName;
+
+    @FXML
+    public void onNameWritten(ActionEvent event) {
+
+    }
+
+}
